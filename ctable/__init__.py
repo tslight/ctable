ffb4e7b6a1804af8af067d34487d7e265c4b0ae6
tslight <tslight@pm.com>
2021-10-20 16:06:56 +0100

    expose more functions

--- ctable/__init__.py
+++ ctable/__init__.py
@@ -1,4 +1,6 @@
 # Copyright (c) 2021, Toby Slight. All rights reserved.
 # ISC License (ISCL) - see LICENSE file for details.
 
+from .ctable import Table
+from .__main__ import show_table
 name = "ctable"
