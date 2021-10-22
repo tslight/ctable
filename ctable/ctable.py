4952d3716a6dab2d55b72a0c46f999c78df18dc0
tslight <tslight@pm.com>
2021-10-22 22:28:38 +0100

    attempting to make things more sophisticated and breaking everything in the process

--- ctable/ctable.py
+++ ctable/ctable.py
@@ -16,22 +16,32 @@ class Table:
     def __init__(self, stdscr, list_of_dicts, column_order, footer=""):
         self.stdscr = stdscr
         self.maxy, self.maxx = self.stdscr.getmaxyx()
-        self.current_row = 0
+        self.currow = 0
+
+        # pad data
+        self.pminrow = 0
+        self.pmincol = 0
+        self.sminrow = 1
+        self.smaxrow = self.maxy - 2
+        self.smaxcol = self.maxx - 1
+
         self.list_of_dicts = list_of_dicts
         self.column_order = column_order
         self.columns = list_of_dicts_to_dict_of_lists(
             self.list_of_dicts, self.column_order
         )
-        self.longest_column_length = get_longest_list_in_dict(self.columns)
-        self.current_row_data = {}
+        self.pmaxrow = get_longest_list_in_dict(self.columns)
+        self.currow_data = {}
         self.color = Color()
         self.keys = {
-            "j, n, DOWN": "Move down a line.",
-            "k, p, UP": "Move up a line.",
-            "g, <, HOME": "Go to first row.",
-            "G, >, END": "Go to last row.",
-            "ENTER/RETURN": "View row data.",
-            "q, ESCAPE": "Exit.",
+            "j, n, DOWN  ": "Move down a row.",
+            "k, p, UP    ": "Move up a row.",
+            "f, d, PGDN  ": "Move down a page.",
+            "b, u, PGUP  ": "Move up a page.",
+            "g, <, HOME  ": "Go to first row.",
+            "G, >, END   ": "Go to last row.",
+            "v, i, ENTER ": "View row data.",
+            "q, x, ESCAPE": "Exit.",
         }
         curses.curs_set(False)  # no cursor
 
@@ -53,23 +63,115 @@ class Table:
         return column_widths
 
     def make_footer(self):
-        footer = curses.newwin(1, self.total_width, self.maxy - 1, 0)
+        footer = curses.newwin(1, self.smincol, self.maxy - 1, 0)
         footer.bkgd(" ", self.color.white_blue)
         footerstr = (
-            f"[{self.current_row}/{self.longest_column_length}]"
-            + "(Press ? or F1 for help)"
+            f"[{self.currow + 1}/{self.pmaxrow}]" + "(Press ? or F1 for help)"
         )
         footer.addstr(0, 0, footerstr)
         footer.noutrefresh()
 
+    def __is_pad_top(self):
+        return self.currow <= 0
+
+    def __is_pad_bottom(self):
+        return self.currow > self.pmaxrow - 3
+
+    def __is_scr_top(self):
+        return self.currow <= self.pminrow
+
+    def __is_scr_bottom(self):
+        return self.currow > self.smaxrow - 2
+
+    def up_pad(self):
+        if self.pminrow > 0:
+            self.pminrow -= 1
+
+    def down_pad(self):
+        if self.pminrow < self.pmaxrow - self.smaxrow:
+            self.pminrow += 1
+
+    def pgdn_pad(self):
+        self.pminrow += self.smaxrow
+        if self.pminrow >= self.pmaxrow - self.smaxrow:
+            self.pminrow = self.pmaxrow - self.smaxrow
+
+    def pgup_pad(self):
+        self.pminrow -= self.maxy - 1
+        if self.pminrow < 0:
+            self.pminrow = 0
+
+    def top_pad(self):
+        self.pminrow = 0
+
+    def bottom_pad(self):
+        self.pminrow = self.pmaxrow - self.smaxrow
+
+    def first_item(self):
+        self.pminrow, self.currow = (0,) * 2
+
+    def last_item(self):
+        self.pminrow = self.pmaxrow - self.smaxrow
+        self.currow = self.pmaxrow - 1
+
+    def down_row(self):
+        if self.currow >= self.pmaxrow - 1:
+            self.first_item()
+            return
+
+        if self.__is_pad_bottom():
+            self.top_pad()
+
+        if self.__is_scr_bottom():
+            self.pminrow += 1
+
+        self.currow += 1  # scroll cursor
+
+    def up_row(self):
+        if self.currow < 1:
+            self.last_item()
+            return
+
+        if self.__is_pad_top():
+            self.bottom_pad()
+
+        if self.__is_scr_top():
+            self.pminrow -= 1
+
+        self.currow -= 1
+
+    def pgdn_row(self):
+        if self.pminrow >= self.pmaxrow - self.smaxrow:
+            self.top_pad()
+        else:
+            self.pminrow += self.smaxrow
+
+        self.currow += self.smaxrow
+
+        if self.currow >= self.pmaxrow - 1:
+            self.first_item()
+
+    def pgup_row(self):
+        if self.pminrow <= 0:
+            self.bottom_pad()
+        else:
+            self.pminrow -= self.smaxrow
+
+        self.currow -= self.smaxrow
+
+        if self.currow < 0:
+            self.last_item()
+
     def make_columns(self, title, items, width):
-        items_win = curses.newpad(self.longest_column_length, width)
+        items_win = curses.newpad(self.pmaxrow, width)
+        # items_win.keypad(True)
+        # items_win.scrollok(True)
+        # items_win.idlok(True)
 
         itemnum = 0
-        pminrow = 0
 
-        if self.current_row > self.maxy - 3:
-            pminrow = self.current_row - self.maxy + 3
+        # if self.currow > self.smaxrow:
+        #     self.pminrow = self.currow - self.maxy + 3
 
         for item in items:
             cellstr = str(item).encode("ascii", "ignore").decode()
@@ -77,26 +179,35 @@ class Table:
             if len(cellstr) >= width:
                 cellstr = f"{cellstr[:width - 2]}.."
 
+            # I know this is really bad practice, but because a running curses
+            # session is almost impossible to debug, it's preferable to see how
+            # things have gone wonky rather than just seeing where things went
+            # wonky.
             try:
                 items_win.addstr(itemnum, 0, cellstr)
             except (curses.error):
                 pass
 
-            if itemnum == self.current_row:
+            if itemnum == self.currow:
                 items_win.chgat(itemnum, 0, self.color.white_magenta_bold)
-                self.current_row_data[title] = item
+                self.currow_data[title] = item
 
             try:
                 items_win.noutrefresh(
-                    pminrow, 0, 1, self.total_width, self.maxy - 2, self.maxx - 1
+                    self.pminrow,
+                    self.pmincol,
+                    self.sminrow,
+                    self.smincol,
+                    self.smaxrow,
+                    self.smaxcol,
                 )
             except (curses.error):
-                pass
+                pass  # Again - I'm sorry programming Gods :-/
 
             itemnum += 1
 
     def make_table(self):
-        self.total_width = 0
+        self.smincol = 0
         column_widths = self.get_column_widths()
         for index, (title, items) in enumerate(self.columns.items()):
             width = column_widths[index]
@@ -105,17 +216,17 @@ class Table:
             if len(titlestr) >= width:
                 titlestr = f"{title[:width - 2]}.."
 
-            title_win = curses.newwin(1, width, 0, self.total_width)
+            title_win = curses.newwin(1, width, 0, self.smincol)
             title_win.bkgd(" ", self.color.white_blue)
 
             try:
                 title_win.addstr(0, 0, titlestr)
             except (curses.error):
-                pass
+                pass  # Yes - I know. I'm a terrible person.
 
             title_win.noutrefresh()
             self.make_columns(title, items, width)
-            self.total_width += width
+            self.smincol += width
 
         self.make_footer()
         curses.doupdate()
@@ -149,20 +260,20 @@ class Table:
             elif key == curses.KEY_RESIZE:
                 self.maxy, self.maxx = self.stdscr.getmaxyx()
                 self.stdscr.erase()
-            elif key == ord("j") or key == curses.KEY_DOWN or key == ord("n"):
-                if self.current_row <= self.longest_column_length - 2:
-                    self.current_row += 1
-            elif key == ord("k") or key == curses.KEY_UP or key == ord("p"):
-                if self.current_row > 0:
-                    self.current_row -= 1
+            elif key == ord("j") or key == ord("n") or key == curses.KEY_DOWN:
+                self.down_row()
+            elif key == ord("k") or key == ord("p") or key == curses.KEY_UP:
+                self.up_row()
+            elif key == ord("f") or key == ord("d") or key == curses.KEY_NPAGE:
+                self.pgdn_row()
+            elif key == ord("b") or key == ord("u") or key == curses.KEY_PPAGE:
+                self.pgup_row()
             elif key == ord("g") or key == ord("<") or key == curses.KEY_HOME:
-                self.current_row = 0
+                self.first_item()
             elif key == ord("G") or key == ord(">") or key == curses.KEY_END:
-                self.current_row = self.longest_column_length - 1
+                self.last_item()
             elif key == ord("\n"):
-                og_dict = dict_from_list_of_dicts(
-                    self.list_of_dicts, self.current_row_data
-                )
+                og_dict = dict_from_list_of_dicts(self.list_of_dicts, self.currow_data)
                 self.init_dict_view(og_dict)
             elif key == ord("?"):
                 self.init_dict_view(self.keys)
