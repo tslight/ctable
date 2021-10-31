61077902018e581d37353a977564f06a34f57232
tslight <tslight@pm.com>
2021-10-31 11:39:54 +0000

    use commit hash as version number

--- setup.py
+++ setup.py
@@ -1,29 +1,44 @@
 # Copyright (c) 2021, Toby Slight. All rights reserved.
 # ISC License (ISCL) - see LICENSE file for details.
 
-import setuptools
+from setuptools import setup, find_packages
+import subprocess
+
+
+def get_commit_hash():
+    """Talk to git and find out the tag/hash of our latest commit"""
+    try:
+        p = subprocess.Popen(
+            ["git", "describe", "--tags", "--dirty", "--always"], stdout=subprocess.PIPE
+        )
+    except EnvironmentError:
+        print("Couldn't run git to get a version number for setup.py")
+        return
+    ver = p.communicate()[0]
+    return ver.strip()
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="ctable",
-    version="0.0.1",
+    version=get_commit_hash(),
     author="Toby Slight",
     author_email="tslight@pm.me",
     description="Curses Tables",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tslight/ctable",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: ISC License (ISCL)",
         "Operating System :: OS Independent",
     ),
     entry_points={
-        'console_scripts': [
-            'ctable = ctable.__main__:main',
+        "console_scripts": [
+            "ctable = ctable.__main__:main",
         ],
-    }
+    },
 )
